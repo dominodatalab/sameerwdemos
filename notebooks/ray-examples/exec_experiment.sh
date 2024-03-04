73c860c73cba2a7830b6b7033faf23dd8080cf1e
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2024-03-04 05:21:52 -0800

    Reorganize notebooks
    Pushed from Domino: https://swaihub38755.cs.domino.tech/workspace/integration-test/mlflow-demos?executionId=65e5934090758361e5bc34d2

--- notebooks/ray-examples/exec_experiment.sh
+++
@@ -1,7 +0,0 @@
-export data_sets_folder=${DOMINO_DATASETS_DIR}/${DOMINO_PROJECT_NAME}
-export num_epochs=5
-export num_trials=5
-export cpus_per_trial=1
-export gpus_per_trial=0
-
-python train.py --storage_path $data_sets_folder  --num_epochs $num_epochs --num_trials $num_trials --cpus_per_trial $cpus_per_trial --gpus_per_trial $gpus_per_trial
